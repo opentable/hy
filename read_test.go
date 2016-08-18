d5ab537fd7fd925b822aabf6dad30fa68532bd15
Sam Salisbury <samsalisbury@gmail.com>
2016-08-18 10:23:33 +0100

    Preserve original values on read.
    - Instead of starting with a blank root node, start with
      the value of the root node supplied.

--- read_test.go
+++ read_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestCodec_Read(t *testing.T) {
+func TestCodec_Read_json_roundTrip(t *testing.T) {
 	jsonWriter := JSONWriter
 	jsonWriter.MarshalFunc = func(v interface{}) ([]byte, error) {
 		return json.MarshalIndent(v, "", "  ")
@@ -40,5 +40,29 @@ func TestCodec_Read(t *testing.T) {
 	}
 	if err := c.Write("testdata/roundtripped2", &v2); err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestCodec_Read_json_preserveOriginal(t *testing.T) {
+	jsonWriter := JSONWriter
+	jsonWriter.MarshalFunc = func(v interface{}) ([]byte, error) {
+		return json.MarshalIndent(v, "", "  ")
+	}
+	c := NewCodec(func(c *Codec) {
+		c.treeReader = NewFileTreeReader("json", "_")
+		c.reader = jsonWriter
+		c.writer = jsonWriter
+	})
+
+	v := TestStruct{
+		unexportedField: "this field should remain",
+	}
+
+	if err := c.Read("testdata/in", &v); err != nil {
+		t.Fatal(err)
+	}
+
+	if v.unexportedField != "this field should remain" {
+		t.Fatal("unexported field not preserved")
 	}
 }
