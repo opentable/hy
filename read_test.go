265300c20dcd3871a3a10e273fd04c48b1ea404d
Sam Salisbury <samsalisbury@gmail.com>
2016-08-20 12:16:15 +0100

    Add test: read FileTargets -> Go values

--- read_test.go
+++ read_test.go
@@ -1,46 +1,94 @@
 package hy
 
 import (
-	"encoding/json"
+	"reflect"
 	"testing"
 
 	"github.com/kr/pretty"
 )
 
-func TestCodec_Read_json(t *testing.T) {
+func TestNode_Read_struct(t *testing.T) {
 	c := NewCodec()
-
-	v := TestStruct{}
-
-	if err := c.Read("testdata/in", &v); err != nil {
+	n, err := c.Analyse(TestStruct{})
+	if err != nil {
 		t.Fatal(err)
 	}
+	reader := c.reader
+	targets := testFileTargets
+	rc := NewReadContext("testdata/in", targets, reader)
+	v := reflect.ValueOf(testDataSimple)
+	val := n.NewValFrom(v)
+	if err := n.Read(rc, val); err != nil {
+		t.Fatal(err)
+	}
+	expectedLen := len(testFileTargetsSnapshot)
+	if targets.Len() != expectedLen {
+		t.Errorf("got len %d; want %d", targets.Len(), expectedLen)
+	}
+	actualTargets := targets.Snapshot()
+	for fileName, actual := range actualTargets {
+		expected, ok := testFileTargetsSnapshot[fileName]
+		if !ok {
+			t.Errorf("extra file generated at %s:\n%s", fileName, actual.TestDump())
+			continue
+		}
+		if actual.Value == nil && expected.Value == nil {
+			continue
+		}
+		var actualType, expectedType reflect.Type
+		if actual.Value != nil {
+			actualType = reflect.ValueOf(actual.Value).Type()
+			if expected.Value == nil {
+				t.Errorf("at %q got: %v; want nil", fileName, actual.Value)
+			}
+		}
+		if expected.Value != nil {
+			expectedType = reflect.ValueOf(expected.Value).Type()
+			if actual.Value == nil {
+				t.Errorf("at %q got: nil; want:\n%v", fileName, expected.Value)
+			}
+		}
 
-	diffs := pretty.Diff(testDataSimple, v)
-	for _, d := range diffs {
-		t.Error(d)
+		if actualType != expectedType {
+			t.Errorf("got type %s; want %s at %q", actualType, expectedType, fileName)
+			t.Errorf("values: got:\n%# v\nwant:\n%# v", actual.Value, expected.Value)
+		}
+		if actual.TestDataDump() != expected.TestDataDump() {
+			t.Errorf("\ngot rendered data:\n%s\nwant:\n%s\n",
+				actual.TestDump(), expected.TestDump())
+		}
+	}
+	for fileName := range testFileTargetsSnapshot {
+		if _, ok := actualTargets[fileName]; !ok {
+			t.Errorf("missing file %q", fileName)
+		}
 	}
 }
 
-func TestCodec_Read_json_preserveOriginal(t *testing.T) {
-	jsonWriter := JSONWriter
-	jsonWriter.MarshalFunc = func(v interface{}) ([]byte, error) {
-		return json.MarshalIndent(v, "", "  ")
-	}
-	c := NewCodec(func(c *Codec) {
-		c.treeReader = NewFileTreeReader("json", "_")
-		c.reader = jsonWriter
-		c.writer = jsonWriter
-	})
+func TestCodec_Read_json(t *testing.T) {
+	c := NewCodec()
 
-	v := TestStruct{
-		unexportedField: "this field should remain",
-	}
+	actual := TestStruct{}
+	expected := testDataSimple
 
-	if err := c.Read("testdata/in", &v); err != nil {
+	if err := c.Read("testdata/in", &actual); err != nil {
 		t.Fatal(err)
 	}
 
+	diffs := pretty.Diff(expected, actual)
+	for _, d := range diffs {
+		t.Error(d, "\n")
+	}
+}
+
+func TestCodec_Read_json_preserveOriginalFieldsWhenNotRead(t *testing.T) {
+	c := NewCodec()
+	v := TestStruct{
+		unexportedField: "this field should remain",
+	}
+	if err := c.Read("testdata/in", &v); err != nil {
+		t.Fatal(err)
+	}
 	if v.unexportedField != "this field should remain" {
 		t.Fatal("unexported field not preserved")
 	}
